ff22a8a5c3c8f80bd317bd106fdc12ac2faf1d56
Ben <ben.talese@gmail.com>
2023-03-21 19:39:46 +1100

    Add IValidationOutputPort

---
+++ Application/Infrastructure/OutputPorts/IValidationOutputPort.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+from typing import Generic
+from Domain.Infrastructure.Generics import TValidationFailure
+
+class IValidationOutputPort(Generic[TValidationFailure], ABC):
+    
+    @abstractmethod
+    def PresentValidationFailure(self, validationFailure: TValidationFailure) -> bool:
+        pass
+    
