d5fe0a54dcfabb9b43f9d6038ffd886f456ce3a8
Ben <ben.talese@gmail.com>
2023-03-23 17:27:03 +1100

    Add interface exception for Python

---
+++ Domain/Errors/InterfaceNotImplementedError.py
@@ -0,0 +1,4 @@
+class InterfaceNotImplementedError(NotImplementedError):
+    def __init__(self, module_name):
+        super().__init__(f"{module_name} does not implement all interface members.")
+        self._code = 5
