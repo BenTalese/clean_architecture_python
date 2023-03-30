419eb15d1276a95104fbf5712095cf26a7540ccc
Ben <ben.talese@gmail.com>
2023-03-30 21:51:30 +1100

    Add test pipe for entity existence

---
+++ Application/UseCases/TestEntity/CreateTestEntity/CreateTestEntityExistenceChecker.py
@@ -0,0 +1,10 @@
+from Application.Infrastructure.Pipes.IEntityExistenceChecker import IEntityExistenceChecker
+from .CreateTestEntityInputPort import CreateTestEntityInputPort
+from .ICreateTestEntityOutputPort import ICreateTestEntityOutputPort
+
+
+class CreateTestEntityExistenceChecker(IEntityExistenceChecker[CreateTestEntityInputPort, ICreateTestEntityOutputPort]):
+
+    def Execute(self, inputPort: CreateTestEntityInputPort, outputPort: ICreateTestEntityOutputPort) -> None:
+
+        self.m_CanInvokeNextPipe = False
--- Application/UseCases/TestEntity/CreateTestEntity/CreateTestEntityInputPort.py
+++ Application/UseCases/TestEntity/CreateTestEntity/CreateTestEntityInputPort.py
@@ -1,6 +1,6 @@
 from Application.Infrastructure.Pipes.IInputPort import IInputPort
 from Application.UseCases.TestEntity.CreateTestEntity.ICreateTestEntityOutputPort import ICreateTestEntityOutputPort
 
-class CreateTestEntityInputPort(ICreateTestEntityOutputPort, IInputPort):
+class CreateTestEntityInputPort(IInputPort):
     def __init__(self, input: str):
         self._input = input
