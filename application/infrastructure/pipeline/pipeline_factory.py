0a2a04d1cc679e34d77283b4da20c289e32b5db9
Ben <ben.talese@gmail.com>
2023-04-13 08:50:12 +1000

    Make pipeline factory import classes by namespace

--- application/infrastructure/pipeline/pipeline_factory.py
+++ application/infrastructure/pipeline/pipeline_factory.py
@@ -1,20 +1,27 @@
 from typing import Dict, List, Type
 from application.infrastructure.pipeline.ipipeline_factory import IPipelineFactory
+from application.infrastructure.pipeline.service_provider import ServiceProvider
+from application.infrastructure.pipeline.wiring import Wiring
 from application.infrastructure.pipes.ipipe import IPipe
 from domain.infrastructure.generics import TInputPort
 
 class PipelineFactory(IPipelineFactory):
     
-    def __init__(self, usecase_registry: Dict[str, List[Type[IPipe]]]):
-        self._usecaseRegistry = usecase_registry if usecase_registry is not None else ValueError(f"'{usecase_registry=}' cannot be None.")
+    def __init__(self, service_provider: ServiceProvider, usecase_registry: Dict[str, List[Type[IPipe]]]):
+        self._service_provider = service_provider if service_provider is not None else ValueError(f"'{service_provider=}' cannot be None.")
+        self._usecase_registry = usecase_registry if usecase_registry is not None else ValueError(f"'{usecase_registry=}' cannot be None.")
 
 
-    def create_pipeline(self, input_port: TInputPort) -> List[Type[IPipe]]:
+    def create_pipeline(self, input_port: TInputPort) -> List[object[IPipe]]:
         _UsecaseKey = input_port.__module__.rsplit(".", 1)[0]
 
-        if _UsecaseKey not in self._usecaseRegistry:
+        if _UsecaseKey not in self._usecase_registry:
             raise KeyError(f"Could not find '{input_port}' in the pipeline registry.")
         
-        _Pipes = self._usecaseRegistry[_UsecaseKey]["pipes"]
+        _PipeNamespaces = self._usecase_registry[_UsecaseKey]["pipes"]
+
+        _PipeClasses = [Wiring.import_class_by_namespace(_Namespace) for _Namespace in _PipeNamespaces]
+
+        _Pipes = [Wiring.get_service(self._service_provider, _PipeClass) for _PipeClass in _PipeClasses]
         
         return sorted(_Pipes, key=lambda _Pipe: _Pipe.priority)
